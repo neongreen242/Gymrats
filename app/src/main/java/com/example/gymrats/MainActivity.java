c4368b93766f204563929b21f585acfc793d431d
Augusto Rodriguez <arod02g@fb.com>
2022-06-21 14:32:26 -0700

    Finishied Compose page for individual users

--- app/src/main/java/com/example/gymrats/MainActivity.java
+++ app/src/main/java/com/example/gymrats/MainActivity.java
@@ -3,17 +3,21 @@ package com.example.gymrats;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.example.gymrats.fragments.ComposeFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationItemView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView bottomNavigationView;
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,22 +32,35 @@ public class MainActivity extends AppCompatActivity {
                 switch (menuItem.getItemId()) {
                     case R.id.action_home:
                         Toast.makeText(MainActivity.this, "Home!!", Toast.LENGTH_SHORT).show();
+                        fragment = new ComposeFragment();
+
                         break;
                     case R.id.action_compose:
                         Toast.makeText(MainActivity.this, "Compose!!", Toast.LENGTH_SHORT).show();
+
+                        fragment = new ComposeFragment();
                         break;
                     case R.id.action_gyms:
                         Toast.makeText(MainActivity.this, "Gyms!!", Toast.LENGTH_SHORT).show();
+                        fragment = new ComposeFragment();
+
                         break;
                     case R.id.action_exercise:
                         Toast.makeText(MainActivity.this, "Exercise!!", Toast.LENGTH_SHORT).show();
+                        fragment = new ComposeFragment();
+
                         break;
                     case R.id.action_profile:
                         Toast.makeText(MainActivity.this, "Profile!!", Toast.LENGTH_SHORT).show();
+                        fragment = new ComposeFragment();
+
                         break;
 
-                    default: break;
+                    default:
+                        fragment = new ComposeFragment();
+                        break;
                 }
+                fragmentManager.beginTransaction().replace(R.id.flContainer,fragment).commit();
                 return true;
 
             }
