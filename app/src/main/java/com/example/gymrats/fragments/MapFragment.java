1c4c785527fb92fb6b5c70e0c0ec191d21dded1d
Augusto Rodriguez <arod02g@fb.com>
2022-07-11 09:41:55 -0700

    Worked on implementation of UI for the login page, opted in for a dark and blue theme. Google Drive:https://docs.google.com/document/d/14hAARa_UmcNFPshC2VLsOT_VRWvlITcpB6vuYRl__Wo/edit?usp=sharing

--- app/src/main/java/com/example/gymrats/fragments/MapFragment.java
+++ app/src/main/java/com/example/gymrats/fragments/MapFragment.java
@@ -33,6 +33,8 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse;
 import com.google.android.libraries.places.api.net.PlacesClient;
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
@@ -71,6 +73,13 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     // Keys for storing activity state.
     private static final String KEY_CAMERA_POSITION = "camera_position";
     private static final String KEY_LOCATION = "location";
+
+    // Used for selecting the current place.
+    private static final int M_MAX_ENTRIES = 5;
+    private String[] likelyPlaceNames;
+    private String[] likelyPlaceAddresses;
+    private List[] likelyPlaceAttributions;
+    private LatLng[] likelyPlaceLatLngs;
 
 
     @Override
@@ -160,13 +169,16 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        map = googleMap;
+        this.map = googleMap;
 
         // Turn on the My Location layer and the related control on the map.
         updateLocationUI();
 
         // Get the current location of the device and set the position of the map.
         getDeviceLocation();
+
+        //Gets nearby fitness centers for the user.
+        //showCurrentPlace();
 
     }
 
@@ -252,6 +264,83 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         }
         super.onSaveInstanceState(outState);
     }
+//
+//    private void showCurrentPlace() {
+//        if (map == null) {
+//            return;
+//        }
+//
+//        if (locationPermissionGranted) {
+//            // Use fields to define the data types to return.
+//            List<Place.Field> placeFields = Arrays.asList(Place.Field.NAME, Place.Field.ADDRESS,
+//                    Place.Field.LAT_LNG);
+//
+//            // Use the builder to create a FindCurrentPlaceRequest.
+//            FindCurrentPlaceRequest request =
+//                    FindCurrentPlaceRequest.newInstance(placeFields);
+//
+//            // Get the likely places - that is, the businesses and other points of interest that
+//            // are the best match for the device's current location.
+//            @SuppressWarnings("MissingPermission") final
+//            Task<FindCurrentPlaceResponse> placeResult =
+//                    placesClient.findCurrentPlace(request);
+//            placeResult.addOnCompleteListener (new OnCompleteListener<FindCurrentPlaceResponse>() {
+//                @Override
+//                public void onComplete(@NonNull Task<FindCurrentPlaceResponse> task) {
+//                    if (task.isSuccessful() && task.getResult() != null) {
+//                        FindCurrentPlaceResponse gyms = task.getResult();
+//
+//                        // Set the count, handling cases where less than 5 entries are returned.
+//                        int count;
+//                        if (gyms.getPlaceLikelihoods().size() < M_MAX_ENTRIES) {
+//                            count = gyms.getPlaceLikelihoods().size();
+//                        } else {
+//                            count = M_MAX_ENTRIES;
+//                        }
+//
+//                        int i = 0;
+//                        likelyPlaceNames = new String[count];
+//                        likelyPlaceAddresses = new String[count];
+//                        likelyPlaceAttributions = new List[count];
+//                        likelyPlaceLatLngs = new LatLng[count];
+//
+//                        for (PlaceLikelihood placeLikelihood : likelyPlaces.getPlaceLikelihoods()) {
+//                            // Build a list of likely places to show the user.
+//                            likelyPlaceNames[i] = placeLikelihood.getPlace().getName();
+//                            likelyPlaceAddresses[i] = placeLikelihood.getPlace().getAddress();
+//                            likelyPlaceAttributions[i] = placeLikelihood.getPlace()
+//                                    .getAttributions();
+//                            likelyPlaceLatLngs[i] = placeLikelihood.getPlace().getLatLng();
+//
+//                            i++;
+//                            if (i > (count - 1)) {
+//                                break;
+//                            }
+//                        }
+//
+//                        // Show a dialog offering the user the list of likely places, and add a
+//                        // marker at the selected place.
+//                        MapsActivityCurrentPlace.this.openPlacesDialog();
+//                    }
+//                    else {
+//                        Log.e(TAG, "Exception: %s", task.getException());
+//                    }
+//                }
+//            });
+//        } else {
+//            // The user has not granted permission.
+//            Log.i(TAG, "The user did not grant location permission.");
+//
+//            // Add a default marker, because the user hasn't selected a place.
+//            map.addMarker(new MarkerOptions()
+//                    .title(getString(R.string.default_info_title))
+//                    .position(defaultLocation)
+//                    .snippet(getString(R.string.default_info_snippet)));
+//
+//            // Prompt the user for permission.
+//            getLocationPermission();
+//        }
+//      }
 
 
 
