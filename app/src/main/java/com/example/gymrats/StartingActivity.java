9b96ae1e743de4193f7ec4d5fe8e774c08ef56a6
Augusto Rodriguez <arod02g@fb.com>
2022-07-11 15:20:55 -0700

    change signup page also added a start screen and added UI elements to both Google Drive: https://docs.google.com/document/d/14hAARa_UmcNFPshC2VLsOT_VRWvlITcpB6vuYRl__Wo/edit

---
+++ app/src/main/java/com/example/gymrats/StartingActivity.java
@@ -0,0 +1,42 @@
+package com.example.gymrats;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.parse.ParseUser;
+
+public class StartingActivity extends AppCompatActivity {
+
+    private Button btnStartSignIn;
+    private Button btnStartSignUp;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_starting);
+
+        btnStartSignIn = findViewById(R.id.btnStartSignIn);
+        btnStartSignUp = findViewById(R.id.btnStartSignUp);
+
+        btnStartSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(StartingActivity.this, SignupActivity.class);
+                startActivity(i);
+            }
+        });
+
+        btnStartSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(StartingActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
